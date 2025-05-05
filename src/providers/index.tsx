bd14b9c7278f9b29655908e1b893ef2ffd494af0
vehktaur <victorakhihiero@gmail.com>
2025-05-05 23:43:01 +0100

    refactor(providers): remove AuthWatcher component from Providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
-import AuthWatcher from '@/components/auth/auth-watcher';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/hooks/queries/query-client';
@@ -10,7 +9,6 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>
-        <AuthWatcher />
         {children}
       </SessionProvider>
 
