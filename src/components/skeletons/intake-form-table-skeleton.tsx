768efd9bdd1c5bb2ca44753ee2c38190a783a181
vehktaur <victorakhihiero@gmail.com>
2025-05-06 14:49:46 +0100

    refactor(intake-form): add IntakeFormTableSkeleton for improved loading state

---
+++ src/components/skeletons/intake-form-table-skeleton.tsx
@@ -0,0 +1,10 @@
+import { Skeleton } from '../ui/skeleton';
+
+const IntakeFormTableSkeleton = () => {
+  return (
+    <div>
+      <Skeleton className="my-4 h-60 w-full rounded-md" />
+    </div>
+  );
+};
+export default IntakeFormTableSkeleton;
