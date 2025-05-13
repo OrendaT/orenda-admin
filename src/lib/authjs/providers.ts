6512e234b9a5a9095dc672781372f88a225c103e
vehktaur <victorakhihiero@gmail.com>
2025-05-13 09:55:18 +0100

    fix(auth): enhance Google authentication flow with error handling and custom error messages

--- src/lib/authjs/providers.ts
+++ src/lib/authjs/providers.ts
@@ -4,6 +4,17 @@ import { LoginSchema } from '../schemas/auth-schema';
 import api from '../api/axios';
 import { AUTH_EP } from '../api/endpoints';
 import { AxiosError } from 'axios';
+import { CredentialsSignin } from '@auth/core/errors';
+
+class CustomError extends CredentialsSignin {
+  code = 'custom';
+
+  constructor(name: string, message: string) {
+    super();
+    this.name = name;
+    this.message = message;
+  }
+}
 
 const providers = [
   Credentials({
@@ -21,7 +32,12 @@ const providers = [
           const res = await api.post(AUTH_EP.LOGIN, validatedFields.data);
           user = res.data;
         } catch (error) {
-          throw error;
+          throw new CustomError(
+            'Login error',
+            error instanceof AxiosError
+              ? error.response?.data?.message
+              : 'Something went wrong',
+          );
         }
       }
 
@@ -31,28 +47,47 @@ const providers = [
   Google({
     profile: async (profile: GoogleProfile) => {
       let user = null;
+      const { email, sub } = profile;
       try {
         const res = await api.post(AUTH_EP.LOGIN, {
-          email: profile.email,
-          sub: profile.sub,
+          email,
+          sub,
         });
         user = res.data;
-      } catch (error) {
+      } catch (loginError) {
         if (
-          error instanceof AxiosError &&
-          error.response?.data.message === 'Login error'
+          loginError instanceof AxiosError &&
+          loginError.response?.data?.message ===
+            'Invalid credentials or inactive user'
         ) {
-          const res = await api.post(AUTH_EP.REGISTER, {
-            email: profile.email,
-            sub: profile.sub,
-          });
-          user = res.data;
+          try {
+            const res = await api.post(AUTH_EP.REGISTER, {
+              email,
+              sub,
+            });
+            user = res.data;
+          } catch (registerError) {
+            // Handle register failure here
+            console.error('Registration failed:', registerError);
+            throw new CustomError(
+              'Google LoginError',
+              'Login and registration both failed.',
+            );
+          }
+        } else {
+          // Handle unexpected login errors (not invalid credentials)
+          console.error('Login failed:', loginError);
+          throw new CustomError(
+            'Google LoginError',
+            loginError instanceof AxiosError
+              ? loginError.response?.data?.message
+              : 'Login failed.',
+          );
         }
       }
 
       return user;
     },
-    authorization: { params: { access_type: 'offline', prompt: 'consent' } },
   }),
 ];
 
