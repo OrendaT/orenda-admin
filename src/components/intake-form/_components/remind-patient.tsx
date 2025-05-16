9f5d5cfa1eff256f43fbda74a107f2da63b6c5e7
vehktaur <victorakhihiero@gmail.com>
2025-05-16 23:21:46 +0100

    refactor(remind-patient): remove unused fields and options from form schema and submission

--- src/components/intake-form/_components/remind-patient.tsx
+++ src/components/intake-form/_components/remind-patient.tsx
@@ -8,20 +8,12 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Status } from '@/types';
 import { sendReminderEmail } from '@/services/email-service';
-import { cn } from '@/lib/utils';
 
 const url = 'https://orenda-intake.vercel.app/';
-
-const options = [
-  { label: 'Email', value: 'email' },
-  { label: 'SMS', value: 'sms' },
-];
 
 const RemindPatientSchema = z.object({
   email: z.string().email({ message: 'Please enter a valid email address' }),
   first_name: z.string().optional(),
-  phone: z.string().optional(),
-  via: z.array(z.enum(['sms', 'email'])).min(1),
 });
 
 const RemindPatient = ({
@@ -33,29 +25,25 @@ const RemindPatient = ({
     defaultValues: {
       email: '',
       first_name: '',
-      phone: '',
-      via: ['email'],
     },
     resolver: zodResolver(RemindPatientSchema),
   });
 
   const {
     handleSubmit,
-    register,
     reset,
     setError,
     formState: { errors, isSubmitting },
   } = methods;
 
   const onSubmit = handleSubmit(async (data) => {
-    const { email, first_name, via } = data;
+    const { email, first_name } = data;
 
     // Using the email service instead of directly calling Mail chimp
     const res = await sendReminderEmail({
       email,
       url,
       first_name,
-      via,
     });
 
     if (res.success) {
@@ -88,14 +76,14 @@ const RemindPatient = ({
           placeholder="Enter email"
         />
 
-        <Input
+        {/* <Input
           label="Phone Number"
           name="phone"
           type="tel"
           placeholder="Enter phone number"
-        />
+        /> */}
 
-        <div>
+        {/* <div>
           <h3 className="mb-2 text-sm font-medium">Send via</h3>
           <div className="flex items-center gap-4">
             {options.map(({ label, value }) => (
@@ -108,7 +96,7 @@ const RemindPatient = ({
                 {label}
 
                 {/* Hidden radio input */}
-                <input
+        {/* <input
                   type="checkbox"
                   value={value}
                   hidden
@@ -116,8 +104,8 @@ const RemindPatient = ({
                 />
               </label>
             ))}
-          </div>
-        </div>
+          </div> */}
+        {/* </div> */}
 
         {errors.root && (
           <p className="error_message !-mt-2 mb-6 ps-2">
