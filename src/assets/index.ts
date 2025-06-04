0993787e3e8e90a85ec6ab9917bad6691f2f5292
vehktaur <victorakhihiero@gmail.com>
2025-06-04 18:57:53 +0100

    feat(skeleton): add PreviewFormSkeleton component for loading state fix(ResourceFile): update image source to use new file asset fix(GeneralTab): replace GeneralTabsSkeleton with PreviewFormSkeleton fix(assets): export new file asset from index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import site_logo from './site_logo.png';
 import arch_banner from './arch_banner.jpg';
 import cover_image from './cover_image.png';
+import file from './file.png';
 
-export { site_logo, arch_banner, cover_image };
+export { site_logo, arch_banner, cover_image, file };
--- src/modules/provider/resources/resource-file.tsx
+++ src/modules/provider/resources/resource-file.tsx
@@ -1,4 +1,4 @@
-import { cover_image } from '@/assets';
+import { file } from '@/assets';
 import type { ResourceFile } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -13,8 +13,8 @@ const ResourceFile = ({
       <article className="h-full w-full items-center justify-center overflow-clip rounded-lg border shadow">
         <div className="flex h-28 w-full border-b bg-[#F6F6F6]">
           <Image
-            className="size-full object-scale-down object-center"
-            src={image || cover_image}
+            className="size-12 object-center m-auto"
+            src={image || file}
             alt="file cover image"
           />
         </div>
