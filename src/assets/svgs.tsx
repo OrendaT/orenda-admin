451f27a4061d36ca6fd596bc2b782bba9b74e89c
vehktaur <victorakhihiero@gmail.com>
2025-07-15 10:30:28 +0100

    increase icon viewbox size

--- src/assets/svgs.tsx
+++ src/assets/svgs.tsx
@@ -189,7 +189,7 @@ export const EmailIcon = (props: SVGProps) => {
 export const IntakeFormsIcon = (props: SVGProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 18 18"
+    viewBox="0 0 24 24"
     fill="none"
     {...props}
   >
@@ -205,7 +205,7 @@ export const ProviderWallIcon = (props: SVGProps) => (
     xmlns="http://www.w3.org/2000/svg"
     width="18"
     height="18"
-    viewBox="0 0 18 18"
+    viewBox="0 0 24 24"
     fill="none"
     {...props}
   >
