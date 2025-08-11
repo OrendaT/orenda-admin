de3fb5e3d1a83aa3870b968fde956dfbee28ccc3
vehktaur <victorakhihiero@gmail.com>
2025-08-11 18:42:52 +0100

    fix url for credentialing forms table

--- src/modules/admin/credentialing/components/credentialing-forms-table/index.tsx
+++ src/modules/admin/credentialing/components/credentialing-forms-table/index.tsx
@@ -5,14 +5,14 @@ import { columns } from './columns';
 import { useAllForms } from '@/hooks/queries/use-all-forms';
 import useFormsParams from '@/hooks/use-forms-params';
 import FormsTable from '@/components/shared/forms-table';
-import { INTAKE_FORMS_EP } from '@/lib/api/endpoints';
+import { CREDENTIALING_FORMS_EP } from '@/lib/api/endpoints';
 import { CredentialingFormData } from '@/types';
 
 const CredentialingFormsTable = () => {
   const { page, search, flag, from, to, status } = useFormsParams();
 
   const { data, isPending, isError } = useAllForms<CredentialingFormData>({
-    url: INTAKE_FORMS_EP.ALL_FORMS,
+    url: CREDENTIALING_FORMS_EP.ALL_FORMS,
     page,
     search,
     filters: {
