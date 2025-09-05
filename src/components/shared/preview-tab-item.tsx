2b44b197661a25c172a7dafa25db923a95af18cf
vehktaur <victorakhihiero@gmail.com>
2025-09-05 13:50:42 +0100

    update

--- src/components/shared/preview-tab-item.tsx
+++ src/components/shared/preview-tab-item.tsx
@@ -2,6 +2,35 @@ import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { BsFiletypePdf } from 'react-icons/bs';
 
+const File = ({
+  isPdf,
+  value,
+  altText,
+}: {
+  isPdf?: boolean;
+  value: string;
+  altText?: string;
+}) => {
+  return isPdf ? (
+    // when value is a pdf
+    <p className="preview_data mt-1.5 mb-2 flex items-center gap-2">
+      <BsFiletypePdf className="size-5 shrink-0" />
+      PDF cannot be displayed. Download it instead
+    </p>
+  ) : (
+    // when value is an image
+    <div className="mb-2 h-40 w-full rounded-md border p-1">
+      <Image
+        className="size-full object-contain"
+        src={value}
+        alt={altText ?? String(name)}
+        width={320}
+        height={240}
+      />
+    </div>
+  );
+};
+
 const TabItem = ({
   name,
   value,
@@ -39,31 +68,21 @@ const TabItem = ({
       {Array.isArray(value) ? (
         // when value is an array
         <ul className="list-disc space-y-1 ps-5">
-          {value.map((item, index) => (
-            <li key={`${name}_${item}_${index}`} className="preview_data">
-              {item}
-            </li>
-          ))}
+          {value.map((item, index) => {
+            const isPdf = isFile && item.endsWith('.pdf');
+            return (
+              <li key={`${name}_${item}_${index}`} className="preview_data">
+                {isFile ? (
+                  <File isPdf={isPdf} value={item} altText={altText} />
+                ) : (
+                  item
+                )}
+              </li>
+            );
+          })}
         </ul>
       ) : isFile ? (
-        isPdf ? (
-          // when value is a pdf
-          <p className="preview_data mt-1.5 mb-2 flex items-center gap-2">
-            <BsFiletypePdf className="size-5 shrink-0" />
-            PDF cannot be displayed. Download it instead
-          </p>
-        ) : (
-          // when value is an image
-          <div className="mb-2 h-40 w-full rounded-md border p-1">
-            <Image
-              className="size-full object-contain"
-              src={value}
-              alt={altText ?? String(name)}
-              width={320}
-              height={240}
-            />
-          </div>
-        )
+        <File isPdf={isPdf} value={value} altText={altText} />
       ) : (
         // when value is a string
         <p className="preview_data">{value}</p>
