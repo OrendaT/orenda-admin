9917a6039b31229cabed99993047fee7b25d071f
vehktaur <victorakhihiero@gmail.com>
2025-09-26 18:24:13 +0100

    update

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,10 +3,13 @@ import { AppSidebar } from '@/components/layout/app-sidebar';
 import ProviderHeader from '@/components/layout/provider-header';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { getUserRole } from '@/lib/utils';
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 const AppLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
+  const _cookies = await cookies();
+  const defaultOpen = _cookies.get('sidebar_state')?.value === 'true';
 
   if (!session) redirect('/login');
 
@@ -14,7 +17,7 @@ const AppLayout = async ({ children }: { children: React.ReactNode }) => {
   const { roles, teams } = session.user;
 
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar roles={roles} teams={teams} />
       <main className="clamp-[p,4,8] w-full bg-[#f6f6f6] pb-[1.31rem]">
         <SidebarTrigger className="mb-4 ml-auto md:hidden" />
